31427c96e03b97ad57559f4dc4320778f165b075
tanushka <bmtanushka@gmail.com>
2023-08-30 17:10:23 +0530

    Update ChatController.php

--- app/Controllers/App/ChatController.php
+++ app/Controllers/App/ChatController.php
@@ -12,15 +12,37 @@ use App\Models\App\ChatModel;
 
 class ChatController extends AuthController
 {
+	protected $chatModel;
+
+	public function __construct() {
+		$this->chatModel = new ChatModel();
+  	}
+	
     public function index()
 	{
         if ( !AuthController::auth() ) {
             return redirect()->route('/');
         }
 
+	$threads = [];
+
+	$response = $this->chatModel->get();
+
+	if ($response->status==200) {
+		$threads = $response->data;
+	}
+
         $pagedata = [
-			'permissions' => $_SESSION['permissions']
-		];
+		    'permissions' => $_SESSION['permissions'],
+	            'pageid' => 'overview',
+	            'title' => 'Messaging Page',
+	            'breadcrumbs' => [ 
+	                'Home' => 'dashboard', 
+	                'User Management' => '', 
+	                'Users' => '' 
+	            ],
+		    'threads' => $threads
+	];
         
 		return view('modules/chat/chats', $pagedata);
 	}
