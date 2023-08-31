ec18e2e242197a875bd0245abd255c0066698479
tanushka <bmtanushka@gmail.com>
2023-08-31 15:34:23 +0530

    Update ChatModel.php

--- app/Models/App/ChatModel.php
+++ app/Models/App/ChatModel.php
@@ -9,5 +9,25 @@ use App\Models\Core\BaseModel;
 
 class ChatModel extends BaseModel
 {
+    #chats
+    public function get()
+    {
+        return json_decode(
+            $this->apiPost('/chats')
+        );
+    }
 
+    public function getChat($data=[])
+    {
+        return json_decode(
+            $this->apiPost('/chats/get', $data)
+        );
+    }
+
+    public function saveChat($data=[])
+    {
+        return json_decode(
+            $this->apiPost('/chats/save', $data)
+        );
+    }
 }
