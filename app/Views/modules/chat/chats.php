e7b5bb56d1bf3d43d04d486efad218f7d0df8456
tanushka <bmtanushka@gmail.com>
2023-09-01 16:20:14 +0530

    Update chats.php

--- app/Views/modules/chat/chats.php
+++ app/Views/modules/chat/chats.php
@@ -81,6 +81,7 @@
                     <!--begin::Card body-->
                     <div class="card-body pt-5" id="kt_chat_contacts_body">
                         <!--begin::List-->
+                        <?php foreach($threads as $thread): ?>
                         <div class="scroll-y me-n5 pe-5 h-200px h-lg-auto" data-kt-scroll="true" data-kt-scroll-activate="{default: false, lg: true}" data-kt-scroll-max-height="auto" data-kt-scroll-dependencies="#kt_header, #kt_app_header, #kt_toolbar, #kt_app_toolbar, #kt_footer, #kt_app_footer, #kt_chat_contacts_header" data-kt-scroll-wrappers="#kt_content, #kt_app_content, #kt_chat_contacts_body" data-kt-scroll-offset="5px">
                             <!--begin::User-->
                             <div class="d-flex flex-stack py-4">
@@ -88,13 +89,13 @@
                                 <div class="d-flex align-items-center">
                                     <!--begin::Avatar-->
                                     <div class="symbol symbol-45px symbol-circle">
-                                        <span class="symbol-label bg-light-danger text-danger fs-6 fw-bolder">M</span>
+                                        <span class="symbol-label bg-light-danger text-danger fs-6 fw-bolder">T</span>
                                     </div>
                                     <!--end::Avatar-->
                                     <!--begin::Details-->
                                     <div class="ms-5">
-                                        <a href="#" class="fs-5 fw-bold text-gray-900 text-hover-primary mb-2">Melody Macy</a>
-                                        <div class="fw-semibold text-muted">melody@altbox.com</div>
+                                        <a href="#" class="fs-5 fw-bold text-gray-900 text-hover-primary mb-2"><?php echo $thread->firstname." ".$thread->lastname; ?></a>
+                                        <div class="fw-semibold text-muted"><?php echo $thread->email; ?></div>
                                     </div>
                                     <!--end::Details-->
                                 </div>
@@ -109,37 +110,8 @@
                             <!--begin::Separator-->
                             <div class="separator separator-dashed d-none"></div>
                             <!--end::Separator-->
-                        </div>
-                        <!--end::List-->
-                        <div class="scroll-y me-n5 pe-5 h-200px h-lg-auto" data-kt-scroll="true" data-kt-scroll-activate="{default: false, lg: true}" data-kt-scroll-max-height="auto" data-kt-scroll-dependencies="#kt_header, #kt_app_header, #kt_toolbar, #kt_app_toolbar, #kt_footer, #kt_app_footer, #kt_chat_contacts_header" data-kt-scroll-wrappers="#kt_content, #kt_app_content, #kt_chat_contacts_body" data-kt-scroll-offset="5px">
-                            <!--begin::User-->
-                            <div class="d-flex flex-stack py-4">
-                                <!--begin::Details-->
-                                <div class="d-flex align-items-center">
-                                    <!--begin::Avatar-->
-                                    <div class="symbol symbol-45px symbol-circle">
-                                        <span class="symbol-label bg-light-danger text-danger fs-6 fw-bolder">M</span>
-                                    </div>
-                                    <!--end::Avatar-->
-                                    <!--begin::Details-->
-                                    <div class="ms-5">
-                                        <a href="#" class="fs-5 fw-bold text-gray-900 text-hover-primary mb-2">Melody Macy</a>
-                                        <div class="fw-semibold text-muted">melody@altbox.com</div>
-                                    </div>
-                                    <!--end::Details-->
-                                </div>
-                                <!--end::Details-->
-                                <!--begin::Lat seen-->
-                                <div class="d-flex flex-column align-items-end ms-2">
-                                    <span class="text-muted fs-7 mb-1">1 week</span>
-                                </div>
-                                <!--end::Lat seen-->
-                            </div>
-                            <!--end::User-->
-                            <!--begin::Separator-->
-                            <div class="separator separator-dashed d-none"></div>
-                            <!--end::Separator-->
-                        </div>
+                        </div>  
+                        <?php endforeach; ?>                        
                         <!--end::List-->
                     </div>
                     <!--end::Card body-->
