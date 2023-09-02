b529ea9dcd882523a8341d6d595e50458135e20c
tanushka <bmtanushka@gmail.com>
2023-09-03 00:45:01 +0530

    Update chats.php

--- app/Views/modules/chat/chats.php
+++ app/Views/modules/chat/chats.php
@@ -155,27 +155,7 @@
                     <!--begin::Card footer-->
                     <div class="card-footer pt-4" id="kt_chat_messenger_footer">
                         <!--begin::Input-->
-                        <textarea class="form-control form-control-flush mb-3" rows="1" data-kt-element="input" placeholder="Type a message"></textarea>
-                        <!--end::Input-->
-                        <!--begin:Toolbar-->
-                        <div class="d-flex flex-stack">
-                            <!--begin::Actions-->
-                            <div class="d-flex align-items-center me-2">
-                                <button class="btn btn-sm btn-icon btn-active-light-primary me-1" type="button" data-bs-toggle="tooltip" title="Coming soon">
-                                    <i class="ki-duotone ki-paper-clip fs-3"></i>
-                                </button>
-                                <button class="btn btn-sm btn-icon btn-active-light-primary me-1" type="button" data-bs-toggle="tooltip" title="Coming soon">
-                                    <i class="ki-duotone ki-exit-up fs-3">
-                                        <span class="path1"></span>
-                                        <span class="path2"></span>
-                                    </i>
-                                </button>
-                            </div>
-                            <!--end::Actions-->
-                            <!--begin::Send-->
-                            <button class="btn btn-primary" type="button" data-kt-element="send">Send</button>
-                            <!--end::Send-->
-                        </div>
+                        
                         <!--end::Toolbar-->
                     </div>
                     <!--end::Card footer-->
