36c0067d7ccefdce23ad8faddf777f94a4560879
tanushka <bmtanushka@gmail.com>
2023-09-03 00:54:25 +0530

    Update chats.php

--- app/Views/modules/chat/chats.php
+++ app/Views/modules/chat/chats.php
@@ -147,7 +147,7 @@
                     <div class="card-body" id="kt_chat_messenger_body">
                         <!--begin::Messages-->
                         <div class="scroll-y me-n5 pe-5 h-300px h-lg-auto" data-kt-element="messages" data-kt-scroll="true" data-kt-scroll-activate="{default: false, lg: true}" data-kt-scroll-max-height="auto" data-kt-scroll-dependencies="#kt_header, #kt_app_header, #kt_app_toolbar, #kt_toolbar, #kt_footer, #kt_app_footer, #kt_chat_messenger_header, #kt_chat_messenger_footer" data-kt-scroll-wrappers="#kt_content, #kt_app_content, #kt_chat_messenger_body" data-kt-scroll-offset="5px">
-                           <img alt="Pic" src="assets/media/begin-chat.png" />
+                           <img alt="Pic" src="assets/media/misc/begin-chat.png" />
                         </div>
                         <!--end::Messages-->
                     </div>
