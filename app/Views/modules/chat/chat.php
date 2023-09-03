55f5fa396de8e0f5ab6e3c7431ca2f1e11a970f6
tanushka <bmtanushka@gmail.com>
2023-09-03 23:21:00 +0530

    Update chat.php

--- app/Views/modules/chat/chat.php
+++ app/Views/modules/chat/chat.php
@@ -81,7 +81,7 @@
                     <!--begin::Card body-->
                     <div class="card-body pt-5" id="kt_chat_contacts_body">
                         <!--begin::List-->
-                        <?php foreach($threads as $thread): ?>
+                        <?php foreach($thread as $msg): ?>
                         <div class="scroll-y me-n5 pe-5 h-200px h-lg-auto" data-kt-scroll="true" data-kt-scroll-activate="{default: false, lg: true}" data-kt-scroll-max-height="auto" data-kt-scroll-dependencies="#kt_header, #kt_app_header, #kt_toolbar, #kt_app_toolbar, #kt_footer, #kt_app_footer, #kt_chat_contacts_header" data-kt-scroll-wrappers="#kt_content, #kt_app_content, #kt_chat_contacts_body" data-kt-scroll-offset="5px">
                             <!--begin::User-->
                             <div class="d-flex flex-stack py-4">
@@ -94,8 +94,8 @@
                                     <!--end::Avatar-->
                                     <!--begin::Details-->
                                     <div class="ms-5">
-                                        <a href="#" class="fs-5 fw-bold text-gray-900 text-hover-primary mb-2"><?php echo $thread->firstname." ".$thread->lastname; ?></a>
-                                        <div class="fw-semibold text-muted"><?php echo $thread->email; ?></div>
+                                        <a href="#" class="fs-5 fw-bold text-gray-900 text-hover-primary mb-2"><?php echo $msg->sender_id." ".$msg->receiver_id; ?></a>
+                                        <div class="fw-semibold text-muted"><?php echo $msg->message_text; ?></div>
                                     </div>
                                     <!--end::Details-->
                                 </div>
@@ -103,7 +103,7 @@
                                 <!--begin::Lat seen-->
                                 <div class="d-flex flex-column align-items-end ms-2">
                                     <?php
-                                        $d1 = new DateTime($thread->timestamp);
+                                        $d1 = new DateTime($msg->timestamp);
                                         $d2 = new DateTime('now');
                                         $interval = $d1->diff($d2);
                                     ?>
