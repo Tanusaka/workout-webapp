c5d4e0500dd3a314942cde8c1c769b83eeb5d6ce
tanushka <bmtanushka@gmail.com>
2023-09-03 23:42:05 +0530

    Update chat.php

--- app/Views/modules/chat/chat.php
+++ app/Views/modules/chat/chat.php
@@ -1,7 +1,7 @@
 <?= $this->extend('layouts/appLayout') ?>
 
 <?= $this->section('content') ?>
-<!--begin::Toolbar--><?= print_r($linked_user); ?><?= print_r($thread); ?>
+<!--begin::Toolbar--><?= print_r($thread); ?><?= print_r($linked_user); ?>
 <div id="kt_app_toolbar" class="app-toolbar py-3 py-lg-6">
     <!--begin::Toolbar container-->
     <div id="kt_app_toolbar_container" class="app-container container-xxl d-flex flex-stack">
