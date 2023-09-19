b57ab3babe5bd2fc6efbe96dbf85b57807befd7c
tanushka <bmtanushka@gmail.com>
2023-09-19 14:47:41 +0530

    test fix

--- app/Controllers/App/ChatController.php
+++ app/Controllers/App/ChatController.php
@@ -121,7 +121,7 @@ class ChatController extends AuthController
 			$this->response->setJSON([ 
 				'status' => $response->status,
 				'redirect' => '',
-				'message'  => $response->message
+				'message'  => $response->message_id
 			]);
 		}
 
